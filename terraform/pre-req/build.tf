279c6008343dc1d2d839127e92facfe698bfa8ae
Craig Thacker <craigthackerx@gmail.com>
2022-09-16 20:23:56 +0100

    update list

--- terraform/pre-req/build.tf
+++ terraform/pre-req/build.tf
@@ -2,7 +2,7 @@ module "rg" {
   source = "registry.terraform.io/libre-devops/rg/azurerm"
 
   rg_name  = "rg-${var.short}-${var.loc}-${terraform.workspace}-build" // rg-ldo-euw-dev-build
-  location = local.location                                          // compares var.loc with the var.regions var to match a long-hand name, in this case, "euw", so "westeurope"
+  location = local.location                                            // compares var.loc with the var.regions var to match a long-hand name, in this case, "euw", so "westeurope"
   tags     = local.tags
 
   #  lock_level = "CanNotDelete" // Do not set this value to skip lock
@@ -38,18 +38,18 @@ module "image" {
         offer     = "azdo-ubuntu"
         sku       = "2204"
       }
+    },
 
-      "lbdo-azdo-windows-2019" = {
-        gallery_name             = module.gallery.gallery_name
-        is_image_specialised     = false
-        image_hyper_v_generation = "V1"
-        image_os_type            = "Windows"
+    "lbdo-azdo-windows-2019" = {
+      gallery_name             = module.gallery.gallery_name
+      is_image_specialised     = false
+      image_hyper_v_generation = "V1"
+      image_os_type            = "Windows"
 
-        identifier = {
-          publisher = "Libre-DevOps"
-          offer     = "azdo-windows"
-          sku       = "2019"
-        }
+      identifier = {
+        publisher = "Libre-DevOps"
+        offer     = "azdo-windows"
+        sku       = "2019"
       }
     }
   }
--- terraform/scale-set/build.tf
+++ terraform/scale-set/build.tf
@@ -90,3 +90,50 @@ module "linux_scale_set" {
   }
 }
 
+module "linux_scale_set" {
+  source = "registry.terraform.io/libre-devops/linux-vm-scale-sets/azurerm"
+
+  rg_name  = data.azurerm_resource_group.data_rg.name
+  location = data.azurerm_resource_group.data_rg.location
+  tags     = data.azurerm_resource_group.data_rg.tags
+
+  ssh_public_key   = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key
+  use_simple_image = false
+  identity_type    = "UserAssigned"
+  identity_ids     = [data.azurerm_user_assigned_identity.mgmt_user_assigned_id.id]
+  asg_name         = "asg-vmss${var.short}${var.loc}${terraform.workspace}-${var.short}-${var.loc}-${terraform.workspace}-01"
+  admin_username   = "LibreDevOpsAdmin"
+
+  settings = {
+    "vmss${var.short}${var.loc}${terraform.workspace}01" = {
+
+      sku                             = "Standard_B4ms"
+      disable_password_authentication = true
+      instances                       = 2
+      overprovision                   = false
+      zones                           = ["2"]
+      provision_vm_agent              = true
+
+      source_image_id = data.azurerm_shared_image.shared_image.id
+
+      os_disk = {
+        storage_account_type = "Standard_LRS"
+        disk_size_gb         = "127"
+      }
+
+      network_interface = {
+        network_security_group_id = module.nsg.nsg_id
+
+        ip_configuration = {
+          subnet_id = element(values(module.network.subnets_ids), 0)
+        }
+      }
+
+      admin_ssh_key = {
+        public_key = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key
+      }
+    }
+  }
+}
+
+
