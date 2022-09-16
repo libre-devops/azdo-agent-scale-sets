e57831be9230ad5875aff9322232e6ba8d25c738
Craig Thacker <craigthackerx@gmail.com>
2022-09-16 23:55:55 +0100

    update build

--- terraform/pre-req/build.tf
+++ terraform/pre-req/build.tf
@@ -112,3 +112,47 @@ module "image" {
     }
   }
 }
+
+module "lnx_vm" {
+  source = "registry.terraform.io/libre-devops/linux-vm/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+
+  vm_amount          = 1
+  vm_hostname        = "lnx${var.short}${var.loc}${terraform.workspace}"
+  vm_size            = "Standard_B2ms"
+  vm_os_simple       = "Ubuntu22.04"
+  vm_os_disk_size_gb = "127"
+
+  asg_name = "asg-${element(regexall("[a-z]+", element(module.lnx_vm.vm_name, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
+
+  admin_username = "LibreDevOpsAdmin"
+  admin_password = data.azurerm_key_vault_secret.mgmt_local_admin_pwd.value
+  ssh_public_key = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key
+
+  subnet_id            = element(values(module.network.subnets_ids), 0)
+  availability_zone    = "alternate"
+  storage_account_type = "Standard_LRS"
+  identity_type        = "SystemAssigned"
+
+  tags = module.rg.rg_tags
+}
+
+module "run_command_lnx" {
+  source = "registry.terraform.io/libre-devops/run-vm-command/azurerm"
+
+  depends_on = [module.lnx_vm] // fetches as a data reference so requires depends-on
+  location   = module.rg.rg_location
+  rg_name    = module.rg.rg_name
+  tags       = module.rg.rg_tags
+
+  vm_name = element(module.lnx_vm.vm_name, 0)
+  os_type = "linux"
+
+  command = "apt-get update && apt-get dist-upgrade -y && apt-get install git unzip zip curl gcc dos2unix -y && apt-get autoclean && exit 0"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
