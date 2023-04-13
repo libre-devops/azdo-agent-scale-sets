33bd6b77f16faa20556a9d2dd4bbc3456941cfd9
Craig Thacker <craigthackerx@gmail.com>
2023-04-13 20:41:42 +0100

    update value

--- terraform/0-pre-req/azuredevops-spoke-provider.tf
+++ terraform/0-pre-req/azuredevops-spoke-provider.tf
@@ -1,3 +1,13 @@
+data "azurerm_key_vault_secret" "azdo_pat" {
+  key_vault_id = data.azurerm_key_vault.mgmt_kv.id
+  name         = var.azdo_pat
+}
+
+data "azurerm_key_vault_secret" "azdo_url" {
+  key_vault_id = data.azurerm_key_vault.mgmt_kv.id
+  name         = var.azdo_url
+}
+
 provider "azuredevops" {
   org_service_url       = data.azurerm_key_vault_secret.azdo_url.value
   personal_access_token = data.azurerm_key_vault_secret.azdo_pat.value
