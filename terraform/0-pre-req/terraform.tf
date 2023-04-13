d1c4bb392cd8ee250426f884e00d78a13d303a62
Craig Thacker <craigthackerx@gmail.com>
2023-04-13 20:39:10 +0100

    update

--- terraform/0-pre-req/terraform.tf
+++ terraform/0-pre-req/terraform.tf
@@ -6,6 +6,11 @@ terraform {
       #      configuration_aliases = [azurerm.default-provider]
       #      version = "~> 2.68.0"
     }
+
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
   }
   backend "azurerm" {
   }
