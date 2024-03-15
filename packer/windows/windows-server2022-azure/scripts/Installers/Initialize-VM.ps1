5797109e15b3804191a175abc3c59039b7b18b7b
Craig Thacker <craigthackerx@gmail.com>
2024-03-15 14:46:39 +0000

    Update

--- packer/windows/windows-server2022-azure/scripts/Installers/Initialize-VM.ps1
+++ packer/windows/windows-server2022-azure/scripts/Installers/Initialize-VM.ps1
@@ -3,6 +3,21 @@
 ##  Desc:  VM initialization script, machine level configuration
 ################################################################################
 
+function Disable-InternetExplorerESC {
+    $adminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $userKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $adminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $userKey -Name "IsInstalled" -Value 0 -Force
+
+    $ieProcess = Get-Process -Name Explorer -ErrorAction SilentlyContinue
+
+    if ($ieProcess) {
+        Stop-Process -Name Explorer -Force -ErrorAction Continue
+    }
+
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled."
+}
+
 function Disable-InternetExplorerWelcomeScreen {
     $AdminKey = "HKLM:\Software\Policies\Microsoft\Internet Explorer\Main"
     New-Item -Path $AdminKey -Value 1 -Force
@@ -15,24 +30,17 @@ function Disable-UserAccessControl {
     Write-Host "User Access Control (UAC) has been disabled."
 }
 
-#function Disable-WindowsUpdate {
-#    $AutoUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
-#    If (Test-Path -Path $AutoUpdatePath) {
-#        Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
-#        Write-Host "Disabled Windows Update"
-#    } else {
-#        Write-Host "Windows Update key does not exist"
-#    }
-#}
 
 # Enable $ErrorActionPreference='Stop' for AllUsersAllHosts
 Add-Content -Path $profile.AllUsersAllHosts -Value '$ErrorActionPreference="Stop"'
 
+
+Write-Host "Disable Server Manager on Logon"
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
+
 Write-Host "Disable 'Allow your PC to be discoverable by other PCs' popup"
 New-Item -Path HKLM:\System\CurrentControlSet\Control\Network -Name NewNetworkWindowOff -Force
 
-#Write-Host "Disable Windows Update"
-#Disable-WindowsUpdate
 
 Write-Host "Disable UAC"
 Disable-UserAccessControl
@@ -51,8 +59,3 @@ Write-Host "Enable long path behavior"
 # See https://docs.microsoft.com/en-us/windows/desktop/fileio/naming-a-file#maximum-path-length-limitation
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
 
-# Expand disk size of OS drive
-$driveLetter = "C"
-$size = Get-PartitionSupportedSize -DriveLetter $driveLetter
-Resize-Partition -DriveLetter $driveLetter -Size $size.SizeMax
-Get-Volume | Select-Object DriveLetter, SizeRemaining, Size | Sort-Object DriveLetter
