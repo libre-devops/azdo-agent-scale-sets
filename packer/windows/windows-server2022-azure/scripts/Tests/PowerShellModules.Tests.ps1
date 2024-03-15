aa2e45c2ed430ac758352cb2aff20deec34f9764
Craig Thacker <craigthackerx@gmail.com>
2024-03-15 01:43:33 +0000

    Update

--- packer/windows/windows-server2022-azure/scripts/Tests/PowerShellModules.Tests.ps1
+++ packer/windows/windows-server2022-azure/scripts/Tests/PowerShellModules.Tests.ps1
@@ -1,23 +1,47 @@
 Describe "PowerShellModules" {
     $modules = (Get-ToolsetContent).powershellModules
-    $withoutVersionsModules = $modules | Where-Object {-not $_.versions} | ForEach-Object {
-        @{moduleName = $_.name}
+    $withoutVersionsModules = $modules | Where-Object { -not$_.versions } | ForEach-Object {
+        @{ moduleName = $_.name }
     }
 
-    $withVersionsModules = $modules | Where-Object {$_.versions} | ForEach-Object {
+    $withVersionsModules = $modules | Where-Object { $_.versions } | ForEach-Object {
         $moduleName = $_.name
         $_.versions | ForEach-Object {
-            @{moduleName = $moduleName; expectedVersion = $_}
+            @{ moduleName = $moduleName; expectedVersion = $_ }
         }
     }
 
     It "<moduleName> is installed" -TestCases $withoutVersionsModules {
-        Get-Module -Name $moduleName -ListAvailable | Should -BeTrue
+        param($moduleName)
+        if ($moduleName -eq "Az")
+        {
+            # Use Get-InstalledModule for the Az module to check if it's installed
+            { Get-InstalledModule -Name $moduleName -ErrorAction SilentlyContinue } | Should -Not -Throw
+            Get-InstalledModule -Name $moduleName -ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
+        }
+        else
+        {
+            # Use Get-Module for other modules
+            Get-Module -Name $moduleName -ListAvailable | Should -Not -BeNullOrEmpty
+        }
     }
 
-    if ($withVersionsModules) {
+    if ($withVersionsModules)
+    {
         It "<moduleName> with <expectedVersion> is installed" -TestCases $withVersionsModules {
-            (Get-Module -Name $moduleName -ListAvailable).Version -contains $expectedVersion | Should -BeTrue
+            param($moduleName, $expectedVersion)
+            if ($moduleName -eq "Az")
+            {
+                # Check version for Az module using Get-InstalledModule
+                $installed = Get-InstalledModule -Name $moduleName -RequiredVersion $expectedVersion -ErrorAction SilentlyContinue
+                $installed.Version.ToString() | Should -BeExactly $expectedVersion
+            }
+            else
+            {
+                # Check version for other modules using Get-Module
+                $version = (Get-Module -Name $moduleName -ListAvailable).Version
+                $version.ToString() | Should -BeExactly $expectedVersion
+            }
         }
     }
 }
