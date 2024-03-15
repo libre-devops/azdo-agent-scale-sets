bc7bd4aec95f861f4deeb5dacaa5a5993d02301a
Craig Thacker <craigthackerx@gmail.com>
2024-03-15 15:39:12 +0000

    Update

--- packer/windows/windows-server2022-azure/scripts/Installers/Initialize-VM.ps1
+++ packer/windows/windows-server2022-azure/scripts/Installers/Initialize-VM.ps1
@@ -3,20 +3,6 @@
 ##  Desc:  VM initialization script, machine level configuration
 ################################################################################
 
-function Disable-InternetExplorerESC {
-    $adminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $userKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $adminKey -Name "IsInstalled" -Value 0 -Force
-    Set-ItemProperty -Path $userKey -Name "IsInstalled" -Value 0 -Force
-
-    $ieProcess = Get-Process -Name Explorer -ErrorAction SilentlyContinue
-
-    if ($ieProcess) {
-        Stop-Process -Name Explorer -Force -ErrorAction Continue
-    }
-
-    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled."
-}
 
 function Disable-InternetExplorerWelcomeScreen {
     $AdminKey = "HKLM:\Software\Policies\Microsoft\Internet Explorer\Main"
@@ -35,9 +21,6 @@ function Disable-UserAccessControl {
 Add-Content -Path $profile.AllUsersAllHosts -Value '$ErrorActionPreference="Stop"'
 
 
-Write-Host "Disable Server Manager on Logon"
-Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
-
 Write-Host "Disable 'Allow your PC to be discoverable by other PCs' popup"
 New-Item -Path HKLM:\System\CurrentControlSet\Control\Network -Name NewNetworkWindowOff -Force
 
@@ -47,9 +30,6 @@ Disable-UserAccessControl
 
 Write-Host "Disable IE Welcome Screen"
 Disable-InternetExplorerWelcomeScreen
-
-Write-Host "Disable IE ESC"
-Disable-InternetExplorerESC
 
 Write-Host "Setting local execution policy"
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine  -ErrorAction Continue | Out-Null
