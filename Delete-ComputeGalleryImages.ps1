ae688c1cb68f48ca471657325c779bbd7b03579e
Craig Thacker <craigthackerx@gmail.com>
2024-03-18 23:29:55 +0000

    Update code

--- Delete-ComputeGalleryImages.ps1
+++ Delete-ComputeGalleryImages.ps1
@@ -35,27 +35,33 @@ function Connect-AzAccountWithServicePrincipal
     }
 }
 
-
 try
 {
     Connect-AzAccountWithServicePrincipal -ApplicationId $ApplicationId -SubscriptionId $SubscriptionId -TenantId $TenantId -Secret $Secret
+
+    # List and delete all managed images in the subscription
+    $managedImages = Get-AzImage
+    foreach ($image in $managedImages)
+    {
+        Write-Host "Deleting managed image: $( $image.Name ) in resource group: $( $image.ResourceGroupName )"
+        # Uncomment the next line to actually perform the deletion
+        # Remove-AzImage -ImageName $image.Name -ResourceGroupName $image.ResourceGroupName -Force
+    }
+
     # List all galleries
     $galleries = Get-AzGallery
-
     foreach ($gallery in $galleries)
     {
         Write-Host "Processing gallery: $( $gallery.Name )"
 
         # List all image definitions within the gallery
         $imageDefinitions = Get-AzGalleryImageDefinition -ResourceGroupName $gallery.ResourceGroupName -GalleryName $gallery.Name
-
         foreach ($imageDef in $imageDefinitions)
         {
             Write-Host "Processing image definition: $( $imageDef.Name )"
 
             # List all image versions for the current image definition
             $imageVersions = Get-AzGalleryImageVersion -ResourceGroupName $gallery.ResourceGroupName -GalleryName $gallery.Name -GalleryImageDefinitionName $imageDef.Name
-
             foreach ($imageVersion in $imageVersions)
             {
                 Write-Host "Deleting image version: $( $imageVersion.Name )"
@@ -67,5 +73,5 @@ try
 }
 catch
 {
-    Write-Error "Error: An exception has occured $_"
+    Write-Error "Error: An exception has occurred $_"
 }
