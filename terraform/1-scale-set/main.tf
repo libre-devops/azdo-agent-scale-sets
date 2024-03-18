cae3edd12ba7bc543da5c94d3d5b8a95ab98ebb6
Craig Thacker <craigthackerx@gmail.com>
2024-03-18 20:01:53 +0000

    Update

--- terraform/1-scale-set/main.tf
+++ terraform/1-scale-set/main.tf
@@ -17,21 +17,21 @@ module "subnet_calculator" {
   subnet_sizes = [26, 26]
 }
 
-module "bastion" {
-  source = "libre-devops/bastion/azurerm"
-
-  rg_name  = data.azurerm_resource_group.rg.name
-  location = data.azurerm_resource_group.rg.location
-  tags     = data.azurerm_resource_group.rg.tags
-
-  bastion_host_name                  = "bst-${var.short}-${var.loc}-${var.env}-01"
-  create_bastion_nsg                 = true
-  create_bastion_nsg_rules           = true
-  create_bastion_subnet              = true
-  bastion_subnet_target_vnet_name    = data.azurerm_virtual_network.vnet.name
-  bastion_subnet_target_vnet_rg_name = data.azurerm_virtual_network.vnet.resource_group_name
-  bastion_subnet_range               = module.subnet_calculator.subnet_ranges[1]
-}
+#module "bastion" {
+#  source = "libre-devops/bastion/azurerm"
+#
+#  rg_name  = data.azurerm_resource_group.rg.name
+#  location = data.azurerm_resource_group.rg.location
+#  tags     = data.azurerm_resource_group.rg.tags
+#
+#  bastion_host_name                  = "bst-${var.short}-${var.loc}-${var.env}-01"
+#  create_bastion_nsg                 = true
+#  create_bastion_nsg_rules           = true
+#  create_bastion_subnet              = true
+#  bastion_subnet_target_vnet_name    = data.azurerm_virtual_network.vnet.name
+#  bastion_subnet_target_vnet_rg_name = data.azurerm_virtual_network.vnet.resource_group_name
+#  bastion_subnet_range               = module.subnet_calculator.subnet_ranges[1]
+#}
 
 locals {
   name = "vmss${var.short}${var.loc}${var.env}01"
@@ -49,57 +49,61 @@ module "azdo_spn" {
   azuredevops_project_name      = data.azurerm_key_vault_secret.azdo_project_name.value
 }
 
-
-module "windows_vm_scale_set" {
-  source = "libre-devops/windows-uniform-orchestration-vm-scale-sets/azurerm"
-
-  rg_name  = data.azurerm_resource_group.rg.name
-  location = data.azurerm_resource_group.rg.location
-  tags     = data.azurerm_resource_group.rg.tags
-
-  scale_sets = [
-    {
-
-      name = local.name
-
-      computer_name_prefix            = "vmss1"
-      admin_username                  = "Local${title(var.short)}${title(var.env)}Admin"
-      admin_password                  = data.azurerm_key_vault_secret.admin_pwd.value
-      instances                       = 1
-      sku                             = "Standard_B2ms"
-      vm_os_simple                    = false
-      use_custom_image                = true
-      custom_source_image_id          = data.azurerm_shared_image.azdo_win_image.id
-      disable_password_authentication = true
-      overprovision                   = true
-      upgrade_mode                    = "Manual"
-      enable_automatic_updates        = true
-      create_asg                      = true
-
-      identity_type     = "SystemAssigned, UserAssigned"
-      identity_ids      = [module.azdo_spn.user_assigned_managed_identity_id]
-      network_interface = [
-        {
-          name                          = "nic-${local.name}"
-          primary                       = true
-          enable_accelerated_networking = false
-          ip_configuration              = [
-            {
-              name                           = "ipconfig-${local.name}"
-              primary                        = true
-              subnet_id                      = data.azurerm_subnet.subnet1.id
-              application_security_group_ids = []
-            }
-          ]
-        }
-      ]
-      os_disk = {
-        caching              = "ReadWrite"
-        storage_account_type = "StandardSSD_LRS"
-        disk_size_gb         = 127
-      }
-
-      extension = []
-    }
-  ]
-}
+#
+#module "windows_vm_scale_set" {
+#  source = "libre-devops/windows-uniform-orchestration-vm-scale-sets/azurerm"
+#
+#  rg_name  = data.azurerm_resource_group.rg.name
+#  location = data.azurerm_resource_group.rg.location
+#  tags     = data.azurerm_resource_group.rg.tags
+#
+#  scale_sets = [
+#    {
+#
+#      name = local.name
+#
+#      computer_name_prefix            = "vmss1"
+#      admin_username                  = "Local${title(var.short)}${title(var.env)}Admin"
+#      admin_password                  = data.azurerm_key_vault_secret.admin_pwd.value
+#      instances                       = 1
+#      sku                             = "Standard_B2ms"
+#      vm_os_simple                    = false
+#      use_custom_image                = true
+#      custom_source_image_id          = data.azurerm_shared_image.azdo_win_image.id
+#      disable_password_authentication = true
+#      overprovision                   = true
+#      upgrade_mode                    = "Manual"
+#      enable_automatic_updates        = true
+#      create_asg                      = true
+#
+#      identity_type     = "SystemAssigned, UserAssigned"
+#      identity_ids      = [module.azdo_spn.user_assigned_managed_identity_id]
+#      network_interface = [
+#        {
+#          name                          = "nic-${local.name}"
+#          primary                       = true
+#          enable_accelerated_networking = false
+#          ip_configuration              = [
+#            {
+#              name                           = "ipconfig-${local.name}"
+#              primary                        = true
+#              subnet_id                      = data.azurerm_subnet.subnet1.id
+#              application_security_group_ids = []
+#            }
+#          ]
+#        }
+#      ]
+#      os_disk = {
+#        caching              = "ReadWrite"
+#        storage_account_type = "StandardSSD_LRS"
+#        disk_size_gb         = 127
+#      }
+#
+#      boot_diagnostics = {
+#        storage_account_uri = null
+#      }
+#
+#      extension = []
+#    }
+#  ]
+#}
